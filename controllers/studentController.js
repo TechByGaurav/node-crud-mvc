9c2b6c02f7150433954c2057d8174017bf8b9e2c
TechByGaurav <gauravyaduvanshi1909@gmail.com>
2025-11-01 13:22:05 +0530

    solve error in controller file

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,8 +1,8 @@
-const student=require('../models/student');
+const Student=require('../models/student');
 
 exports.getAllStudents=async(req,res)=>
 {
-    const students=await student.find();
+    const students=await Student.find();
     res.render('index',{students});
 }
 
@@ -14,23 +14,24 @@ exports.addStudentForm=(req,res)=>
 exports.addStudent=async(req,res)=>
 {
     const{name,email,age}=req.body;
-    await student.create({name,email,age});
+    await Student.create({name,email,age});
     res.redirect('/')
 }
 
 exports.editStudentForm=async(req,res)=>
 {
-    const student=await student.findById(req.params.id);
+    const student=await Student.findById(req.params.id);
     res.render('edit',{student});
 }
 
 exports.updateStudent=async(req,res)=>
 {
     const{name,email,age}=req.body;
-    await student.findByIdAndUpdate(req.params.id,{name,email,age});
+    await Student.findByIdAndUpdate(req.params.id,{name,email,age});
     res.redirect('/');
 }
 exports.deleteStudent=async(req,res)=>
 {
-    await student.findByIdAndUpdate(req.params.id);
+    await Student.findByIdAndDelete(req.params.id);
+    res.redirect('/')
 }
